cddbb385fbd230278828213a681a2d708635cf60
Wei-Kai Fang <weikaaai@gmail.com>
2020-12-03 11:06:26 +0800

    calculate fibonacci sequence with iterative method

---
+++ fib.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+int fib(int num)
+{
+    if(num < 0) return -1;
+    if(num <= 1) return num;
+    int *res = (int*)malloc((num + 1) * sizeof(int));
+    res[0] = 0;
+    res[1] = 1;
+    for(int i = 2; i <= num ; i++)
+    {
+        res[i] = res[i - 1] + res[i - 2];
+    }
+    return res[num];
+}
