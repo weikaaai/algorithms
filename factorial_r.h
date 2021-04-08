be2659662d7b1555b60d9eb458069a5e6ed5c8d5
Wei-Kai Fang <weikaaai@gmail.com>
2021-04-08 14:51:54 +0800

    files deleted

--- factorial_r.h
+++
@@ -1,6 +0,0 @@
-int factorial_r(int num)
-{
-    if(num < 0) return -1;
-    if(num <= 1) return 1;
-    return factorial_r(num - 1) * num;
-}
--- fib_r.h
+++
@@ -1,6 +0,0 @@
-int fib_r(int num)
-{
-    if(num < 0) return -1;
-    if(num <= 1) return num;
-    return fib_r(num - 1) + fib_r(num - 2);
-}
